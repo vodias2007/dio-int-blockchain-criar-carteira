53ba9331004964a7d487fa320258314ebc21cadf
vodias2007 <104235386+vodias2007@users.noreply.github.com>
2022-08-24 13:03:31 -0300

    Add files via upload

---
+++ createWallet.js
@@ -0,0 +1,34 @@
+//importando as dependencias
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definir a rede
+const network = bitcoin.networks.testnet
+
+//derivacao de carteiras HD: 1 pra testnet e 0 pra mainnet
+const path = `m/49'/1'/0'/0`
+
+//criando o mnemonic pra seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+//criando uma conta (par de pvt+pub keys)
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network
+
+
+
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada:", node.toWIF())
+console.log("Seed", mnemonic)
